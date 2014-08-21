bdcb1094a7f95695b657487615b47a1de04308f5
Jim Hayes <jhayes@sdsc.edu>
2014-08-21 14:52:45 -0700

    New version.  Clean up build process.

--- src/polymake/version.mk
+++ src/polymake/version.mk
@@ -1,16 +1,14 @@
-PKGROOT            = /opt/polymake
 NAME               = polymake
-VERSION            = 2.12
-RELEASE            = 2
+VERSION            = 2.13.1
+RELEASE            = 0
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = polymake
 
 SOURCE_NAME        = $(NAME)
-SOURCE_VERSION     = 2.12
-SOURCE_RC_VERSION  = 2.12-rc3
+SOURCE_VERSION     = 2.13-1
 SOURCE_SUFFIX      = tar.bz2
-SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_RC_VERSION).$(SOURCE_SUFFIX)
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_NAME)-$(SOURCE_VERSION)
 
 TAR_BZ2_PKGS           = $(SOURCE_PKG)
