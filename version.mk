ead1406b65797f6c87cb69c6da8009badc0f3017
Jerry Greenberg <jpg@comet-devel.local>
2015-03-21 21:16:57 -0700

    put in auto versioning

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
-ROLLNAME	= polymake
-VERSION		= 1.0
-RELEASE		= 0
-COLOR		= firebrick
+ROLLNAME        = polymake
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT	= $(CURDIR)
+REDHAT.ROOT     = $(PWD)
