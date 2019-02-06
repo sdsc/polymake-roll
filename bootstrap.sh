200d90c39c3ec3bc22826560eff9ce1dc3e1cd8d
Jerry Greenberg <jpg@sdsc.edu>
2019-02-06 11:56:41 -0800

    APS-991 removed .noarch from yum perl package names in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 # Install additional system packages required for roll build.
-yum install perl-ExtUtils-Embed.noarch
-yum install perl-XML-Writer.noarch
+yum install perl-ExtUtils-Embed
+yum install perl-XML-Writer
